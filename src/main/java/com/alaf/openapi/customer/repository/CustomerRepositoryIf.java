00c883721779ec36d288689b08519a55e8590e24
unknown <it.zvision@yahoo.com>
2024-01-29 19:38:36 +0100

    removed unused dependency, replaced Repository

--- src/main/java/com/alaf/openapi/customer/repository/CustomerRepositoryIf.java
+++ src/main/java/com/alaf/openapi/customer/repository/CustomerRepositoryIf.java
@@ -1,12 +1,13 @@
 package com.alaf.openapi.customer.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.alaf.openapi.customer.model.Customer;
 
 @Repository
-public interface CustomerRepositoryIf extends CrudRepository<Customer, Long> {
+public interface CustomerRepositoryIf extends JpaRepository<Customer, Long> {
 
 	
 }
